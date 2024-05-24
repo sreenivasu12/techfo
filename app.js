3999ff21eb140257f7c63f01fb3b8a2d00e673ce
sreenivasu12 <168365041+sreenivasu12@users.noreply.github.com>
2024-05-24 08:10:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+const array=(...arr)=>console.log(arr);
+array(1,3,4,5,6,8)
---
+++ javascrip.js
@@ -0,0 +1,9 @@
+set1= {
+    name:"nbv",
+    email:"mals12@gmail.com"
+}
+set2= {
+    ...set1,
+    age:32
+}
+console.log(set2);
